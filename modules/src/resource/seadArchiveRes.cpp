61c0ccbcb2db2c378d77edd29bae3c5d1c380d34
ud2 <sjx233@qq.com>
2025-07-17 03:39:43 +0800

    wip

--- modules/src/resource/seadArchiveRes.cpp
+++ modules/src/resource/seadArchiveRes.cpp
@@ -11,9 +11,4 @@ void ArchiveRes::doCreate_(u8* buf, u32, Heap*)
 {
     mEnable = prepareArchive_(buf);
 }
-
-bool ArchiveRes::isExistFileImpl_(const SafeString& path) const
-{
-    return convertPathToEntryIDImpl_(path) != -1;
-}
 }  // namespace sead
